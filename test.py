2f2aff32a646db207711bbd1d330a11899c1fb7f
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2023-05-26 11:48:52 -0700

    Moved summary to the top

--- test.py
+++ test.py
@@ -20,22 +20,23 @@ total = 0
 
 list = glob.glob("tests/single-exec/*.c")
 
-print("[Click for summary](#summary)\n\n")
-
-print("## Raw results\n")
+strs = []
 
 for f in list:
 	total += 1
 	result = subprocess.run(["sh", "./runners/single-exec/cuik-x86_64", f], capture_output=True, text=True)
 
-	print(f"# Test `{f}`\n")
-	print("```\n")
-	print(ansi_escape.sub('', result.stdout))
-	print(ansi_escape.sub('', result.stderr))
-	print("```\n")
+	strs.append(f"# Test `{f}`\n")
+	strs.append("```\n")
+	strs.append(ansi_escape.sub('', result.stdout))
+	strs.append(ansi_escape.sub('', result.stderr))
+	strs.append("```\n")
 
 	if result.returncode == 0:
 		good += 1
 
 print("## Summary\n")
-print(f"passed {good} out of {total}")
+print(f"passed {good} out of {total}\n\n")
+
+print("## Raw results\n")
+print("\n\n".join(strs))
