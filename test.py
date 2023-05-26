97b30d53bcc60ac308af9768530dae2d8bb6ef9a
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2023-05-26 11:31:11 -0700

    Flavorful

--- test.py
+++ test.py
@@ -6,18 +6,22 @@ total = 0
 
 list = glob.glob("tests/single-exec/*.c")
 
-print("[Click for summary](#summary)")
+print("[Click for summary](#summary)\n\n")
 
 print("## Raw results\n")
-print("```\n")
 
 for f in list:
 	total += 1
-	result = subprocess.run(["sh", "./runners/single-exec/cuik-x86_64", f])
+	result = subprocess.run(["sh", "./runners/single-exec/cuik-x86_64", f], capture_output=True)
+
+	print(f"# Test `{f}`\n")
+	print("```\n")
+	print(result.stdout.decode('utf-8'))
+	print(result.stderr.decode('utf-8'))
+	print("```\n")
+
 	if result.returncode == 0:
 		good += 1
-
-print("```\n")
 
 print("## Summary\n")
 print(f"passed {good} out of {total}")
