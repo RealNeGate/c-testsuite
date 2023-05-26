a86f4612dfe3386e1abccee288019a05a7d7f46b
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2023-05-26 11:21:01 -0700

    Hopefully Prettifying the markdown

--- test.py
+++ test.py
@@ -6,10 +6,18 @@ total = 0
 
 list = glob.glob("tests/single-exec/*.c")
 
+print("[Click for summary](#summary)")
+
+print("## Raw results ")
+print("```\n")
+
 for f in list:
 	total += 1
 	result = subprocess.run(["sh", "./runners/single-exec/cuik-x86_64", f])
 	if result.returncode == 0:
 		good += 1
 
-print(f"Summary:\n  passed {good} out of {total}")
+print("```\n")
+
+print("## Summary ")
+print(f"passed {good} out of {total}")
